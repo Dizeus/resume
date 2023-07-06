68e7f37e29a1c9f189488401ade242366d7e01c9
Kharchuk Illia <kharchukillia@gmail.com>
2023-07-06 18:19:47 +0300

    Add header and About block version 1.0

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
+const links = document.getElementsByClassName('header__link');
+const onHeaderLinkClick = (event)=>{
+	if(event.target.classList.value !== 'header__link active'){
+		[...links].forEach(link => {
+			link.classList.value === 'header__link active' && link.classList.remove('active')
+		});
+		event.target.classList.add('active');
+	}
+}
 
