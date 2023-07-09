7fc57ad7282724fde3036ce3b1da06a5bd7663fa
Kharchuk Illia <kharchukillia@gmail.com>
2023-07-09 16:08:58 +0300

    Add skills section -version 1.1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 const links = document.getElementsByClassName('header__link');
 const onHeaderLinkClick = (event)=>{
-	if(event.target.classList.value !== 'header__link active'){
+	if(event.target.classList.value !== 'header__link header__link_active'){
 		[...links].forEach(link => {
-			link.classList.value === 'header__link active' && link.classList.remove('active')
+			link.classList.value === 'header__link header__link_active' && link.classList.remove('header__link_active')
 		});
-		event.target.classList.add('active');
+		event.target.classList.add('header__link_active');
 	}
 }
 
+const onMenuBurgerClick = () =>{
+	document.querySelector(".header__burger").classList.toggle('active');
+	document.querySelector(".header__menu-body").classList.toggle('active');
+	document.body.classList.toggle("lock");
+}
